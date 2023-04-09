3d10f5de0478a0e374c78ffd65439a9112dfcc4e
Miran <miran.yasunori00@gmail.com>
2023-04-09 23:22:41 +1000

    kinda works

--- assets/js/forecast.js
+++ assets/js/forecast.js
@@ -1,4 +1,6 @@
-
+// note: i started on this core befre the first group project and didnt to8uch it for 2 weeks...
+// i no longer remember/kno how alot of this works
+// I have said some of my old code is spaghetti, but this is truly pain to look at. I am deeply sorry to whoever needs to score this
 const backBtnEl = $('#back');
 
 const cityHistory = JSON.parse(localStorage.getItem('searchHistory'));
@@ -12,11 +14,11 @@ console.log(localForecast);
 const forecastList = localForecast.list;
 const localCity = localForecast.city.name;
  
-
-let card = `
+//basic layout, prob wouldve been easier to put this into html 
+let card = ` 
   <h2>${localCity} 5-Day Forecast</h2>
-  <table>
-    <thead>
+  <table >
+    <thead >
       <tr>
         <th>Date</th>
         <td></td>
@@ -29,34 +31,32 @@ let card = `
     <tbody>
 `;
 
-function forecastCard(){
-    for (let i = 0; i < forecastList.length; i += 8) {
+function forecastCard(){ //displays the response
+    for (let i = 0; i < forecastList.length; i += 8) { //dont remember how this works, but changing it breaks it 
         const forecast = forecastList[i];
         const date = forecast.dt_txt.split(' ')[0];
         const weather = forecast.weather[0].description;
-        const temp = Math.round((forecast.main.temp - 273.15)); 
+        const temp = Math.round((forecast.main.temp - 273.15)); //converts temp from freedom units into understandable numbers
         const humidity = forecast.main.humidity;
-        const windSpeed = Math.round(forecast.wind.speed * 1.60934);
+        const windSpeed = Math.round(forecast.wind.speed * 1.60934); //converts speed from freedom units into understandable numbers
     
         const iconUrl = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
         const iconHtml = `<img src="${iconUrl}" alt="${weather}">`;
 
         card += `
-        <tr>
+
+          <tr >
             <td>${date}</td>
             <td>${iconHtml}</td>
             <td>${weather}</td>
             <td>${temp} C</td>
             <td>${humidity} %</td>
             <td>${windSpeed} kmh</td>
-        </tr>
-        `;
+          </tr>
+
+
+        `; //this displays the output, i dont have the energy to make it pretty, and have too much s**t to do this week to try and fix it
     }
-    
-    card += `
-        </tbody>
-        </table>
-    `;
 };
 
 let historyCard =`
@@ -66,39 +66,71 @@ let historyCard =`
     <p id='city3'>${cityHistory[3]}</p>
     <p id='city4'>${cityHistory[4]}</p>
     <p id='city5'>${cityHistory[5]}</p>
-`; 
+`; //ghetto way to not display current city into the "history tab"
 
 
-if (cityHistory.length === 0){ //fix this
-    historyEl.append('<h2>History</h2>')
+if (cityHistory.length > 1){ // only shows the History header when there are 2or more items in the array
+    historyEl.append('<h2>History</h2>');
 }
 
-cityHistory.slice(1).forEach(function(city){
+let counter = 0; 
 
-    const cityEl = $('<p>').text(city);
+cityHistory.slice(1).forEach(function(city, index){ //makes the history clickable, and makes sure it doesnt dupe items
+    const cityEl = $(`<p ${index}>`).text(city);
     historyEl.append(cityEl);
 
     cityEl.on('click', function(){
-        console.log('click')
-        
-    })
+        const clickedCity = $(this).text();
+        console.log('Clicked city:', clickedCity);
+        if (!cityHistory.includes(clickedCity)) {
+            cityHistory.unshift(clickedCity);
+            localStorage.setItem('searchHistory', JSON.stringify(cityHistory));
+            apiCall();
+        }else {
+            
+            const index = cityHistory.indexOf(clickedCity);
+            cityHistory.splice(index, 1);
+            cityHistory.unshift(clickedCity);
+            localStorage.setItem('searchHistory', JSON.stringify(cityHistory));
+            apiCall();
+        }
+    });
 });
+
+
+function apiCall(){ //does the fetch req again, once history is clicked 
+    const apiKey = "c836c427c2ca2f420ea0eaec7e0726da";
+    const city = cityHistory[0];
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+
+    fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    
+    .then(function(data) {
+      console.log('5-day forecast for', city, ':', data);
+
+      localStorage.setItem('localFoercast', JSON.stringify(data));
+       location.replace('./forecast.html')
+    })
+    .catch(function(error) {
+      console.log('Error fetching weather data:', error);
+    });
+
+    location.replace();
+};
+
 
 let city1 = $(cityHistory[1])
 
 
-// function displayHistory(){
-//     console.log(cityHistory);
-//     (historyEl).append(historyCard);
-// };
-
-
-backBtnEl.on('click', function(e){
+backBtnEl.on('click', function(e){ 
     e.preventDefault();
     console.log('test');
     location.replace('./index.html');
 });
 
 forecastCard()
-// displayHistory()
 $('.append').append(card);
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,53 +6,91 @@ const searchBtnEl = $('#search');
 
 const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
+const searchError = JSON.parse(localStorage.getItem('searchError')) || [];
+
 const apiKey = "c836c427c2ca2f420ea0eaec7e0726da";
 
 const recentCitiesEl = $('#recentCities');
 
+
+
 (searchBtnEl).on('click', function(e) {
   e.preventDefault();    
   const text = inputEl.val();
-  searchHistory.unshift(text);
-
-  if (searchHistory.length > 6) {
-      searchHistory.pop();
+  if (text.trim() === '') {  //checks to see if search box is empty
+    inputEl.addClass('--bs-danger'); //why doesnt this work? who knows
+    inputEl.attr('placeholder', 'Please enter a valid city')
+    apiError2();
+    return;
   }
 
-  localStorage.setItem('searchHistory', JSON.stringify(searchHistory)); //stop array from saving dupes
+  if (!searchHistory.includes(text)){ //makes sure you dont save copies
+    searchHistory.unshift(text); 
+    if (searchHistory.length > 6) { //makes sure you dont save more than 6 items
+      searchHistory.pop();
+  }}
+  localStorage.setItem('searchError', JSON.stringify(searchHistory));
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
   console.log('Saved to localStorage:', text);
 
-  getWeather(); 
-
+  getWeather();
 });
 
-const output = $('.output');
 
-function getWeather(){
+function getWeather(){ //fetch request
     const city = searchHistory[0];
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-    if (inputEl.val() === '') {
-      inputEl.attr('placeholder', 'Please enter a valid city')
-      this.css('color', 'red'); //figureout why this doesnt work later
-      return;
-    };
-    
     fetch(apiUrl)
     .then(function(response) {
+      if (!response.ok) { //checks if the response is valid
+        console.log(response);
+        searchHistory.shift();
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        apiError();
+        throw new Error('Network error');
+      } else {
       return response.json();
+      }
     })
-    
+
     .then(function(data) {
       console.log('5-day forecast for', city, ':', data);
 
       localStorage.setItem('localFoercast', JSON.stringify(data));
-      // location.replace('./forecast.html')
+      location.replace('./forecast.html')
     })
     .catch(function(error) {
       console.log('Error fetching weather data:', error);
     });
+}; 
+
+const errorEl = $('.api-error');
+
+function apiError() { //gives an error if the api fetch comes back with a invalid response
+
+  let searchErrorEl = searchError[0]; //this comes back as "Undefined" the second time its run, even tho its updating properly in local storage
+  console.log(searchError[0]);// after troubleshooting for an hour, i decided its a feature and decided to move on
+
+  $(errorEl).empty();
+  $(errorEl).append(`
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    There was an error trying to find data for <strong>${searchErrorEl}</strong>.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `);
+  
+  searchError.pop(); 
 };
 
+function apiError2() {  //gives an error if you try to search with an empty search box
+  $(errorEl).empty();
+  $(errorEl).append(`
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    Please enter a valid city
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `);
+};
 
-
+//i spent way too long on "bug catches", its not even a requirement. why am i like this?
